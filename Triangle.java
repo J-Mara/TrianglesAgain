4a9cfc97704baf8205e454c35dddb0234c2d4bd5
J-Mara <jmaragakis20@stuy.edu>
2020-11-01 16:14:59 -0500

    Triangle with area and perimiter

--- Triangle.java
+++ Triangle.java
@@ -19,4 +19,11 @@ public class Triangle{
     return v1.distanceTo(v2) + v2.distanceTo(v3) + v3.distanceTo(v1);
   }
 
+  public double getArea(){
+    double S = getPerimeter()/2;
+    return Math.sqrt(S * (S - v1.distanceTo(v2)) *
+    (S - v2.distanceTo(v3)) * 
+    (S - v3.distanceTo(v1)));
+  }
+
 }
