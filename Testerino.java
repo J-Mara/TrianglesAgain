b70a19317d053ebda48d0cbe4af1cbc21424d3bd
J-Mara <jmaragakis20@stuy.edu>
2020-11-01 16:51:23 -0500

    Tester with trials for everything

--- Testerino.java
+++ Testerino.java
@@ -2,9 +2,30 @@ public class Testerino{
   public static void main(String[] args) {
     Point p1 = new Point(2, 5);
     Point p2 = new Point(6, 2);
+    Point p3 = new Point(6, 5);
+    Point p4 = new Point(6, 8);
+    Point p5 = new Point(3, 3);
+    Triangle t1 = new Triangle(p1, p2, p3);
+    Triangle t2 = new Triangle(p1, p2, p4);
 
     System.out.println(p1.distanceTo(p2));
 
     System.out.println(p1.equals(p2));
+
+    System.out.println(t1.getPerimeter());
+
+    System.out.println(t1.getArea());
+
+    System.out.println(t1.classify());
+    System.out.println(t2.classify());
+
+    System.out.println(t1.toString());
+    System.out.println(t2.toString());
+
+    t1.setVertex(0, p5);
+    t2.setVertex(2, p5);
+
+    System.out.println(t1.toString());
+    System.out.println(t2.toString());
   }
 }
