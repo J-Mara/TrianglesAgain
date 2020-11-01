84ad03355f0d4ec6932a1626d21db6b3951bcdc1
J-Mara <jmaragakis20@stuy.edu>
2020-11-01 14:53:36 -0500

    tester with 2 points

---
+++ Testerino.java
@@ -0,0 +1,8 @@
+public class Testerino{
+  public static void main(String[] args) {
+    Point p1 = new Point(2, 5);
+    Point p2 = new Point(6, 2);
+
+    System.out.println(p1.distanceTo(p2));
+  }
+}
